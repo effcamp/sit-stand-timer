3c7358347a7cbae98467c0f3fc3237541aab0745
effcamp <fcf.camposx@gmail.com>
2018-01-06 16:36:18 -0200

    Refactored and removed experimental code. Notification pops up

--- src/components/App.js
+++ src/components/App.js
@@ -5,43 +5,72 @@ import '../styles/App.css';
 class App extends Component {
   state = {
     count: 0,
-    time: moment().format('MMMM Do YYYY, h:mm:ss a')
+    time: moment().format('MMMM Do YYYY, h:mm:ss a'),
+    stand: undefined,
+    sit: undefined,
+    notiOptions: {
+      title: 'Stand up!',
+      body: 'Walk around for the next 5 minutes'
+    }
   };
+
   componentDidMount() {
-    this.timeInterval = setInterval(() => {
+    this.interval = setInterval(() => {
       this.setState(() => ({
         time: moment().format('MMMM Do YYYY, h:mm:ss a')
       }));
     }, 1000);
   }
-  updateCounter = () => {
-    this.setState((prevState) => ({
-      count: prevState.count + 1
-    }));
-  };
-  startCounter = () => {
-    this.interval = setInterval(() => {
-      this.updateCounter();
-    }, 1000);
-  };
-  stopCounter = () => {
-    clearInterval(this.interval);
-    clearInterval(this.timeInterval);
-  };
 
   componentWillUnmount() {
     clearInterval(this.interval);
-    clearInterval(this.timeInterval);
   }
+
+  startTimer = () => {
+    this.setState((prevState) => ({
+      stand: moment()
+        .add(5, 'seconds')
+        .format('MMMM Do YYYY, h:mm:ss a'),
+      sit: moment()
+        .add(10, 'seconds')
+        .format('MMMM Do YYYY, h:mm:ss a')
+    }));
+  };
+
+  stopTimer = () => {
+    this.setState(() => ({
+      stand: undefined,
+      sit: undefined
+    }));
+  };
+
+  showMessage = () => {
+    if (this.state.time === this.state.stand) {
+      const notify = new Notification('Stand up!', this.state.notiOptions);
+      setTimeout(() => {
+        notify.close();
+      }, 5000);
+    }
+    if (
+      this.state.time >= this.state.stand &&
+      this.state.time <= this.state.sit
+    ) {
+      return <h1>STANDUP!</h1>;
+    } else {
+      return <h1>Sitting time</h1>;
+    }
+  };
 
   render() {
     return (
       <div className="App">
         <div className="container">
-          <p>{this.state.count}</p>
-          <button onClick={this.startCounter}>Start</button>
-          <button onClick={this.stopCounter}>Stop</button>
+          {this.showMessage()}
           <p>{this.state.time}</p>
+          <p>stand at: {this.state.stand}</p>
+          <p>sit at: {this.state.sit}</p>
+          <button onClick={this.startTimer}>Start</button>
+          <button onClick={this.stopTimer}>Stop</button>
         </div>
       </div>
     );
